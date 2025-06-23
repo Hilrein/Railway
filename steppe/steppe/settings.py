0751c927c2ca129a9130c57f18fe81d8a912aaec
Sanzhar Turabaev <167844412+Hilrein@users.noreply.github.com>
2025-06-23 23:48:22 +0500

    commit render

--- steppe/steppe/settings.py
+++ steppe/steppe/settings.py
@@ -4,9 +4,19 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4rbv43o=0tj8q)_n0$@!5j%m1i@-8)@$f_h68rray+g2vu3_nm')
-DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'    
+DEBUG = os.environ.get('RENDER', 'False') != 'true'                     
+# DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'    
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app', 'wh029ek8lu.onrender.com', '.onrender.com']
+ALLOWED_HOSTS = []
+
+
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+    # ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.railway.app', 'wh029ek8lu.onrender.com', '.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'https://*.onrender.com']
 
 INSTALLED_APPS = [
